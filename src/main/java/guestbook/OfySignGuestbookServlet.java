526c5f7480c6758be18f9d90662627afd69eccb0
Kan <kanvan@utexas.edu>
2019-10-31 22:06:54 -0500

    first

---
+++ src/main/java/guestbook/OfySignGuestbookServlet.java
@@ -0,0 +1,58 @@
+package guestbook;
+
+
+import com.googlecode.objectify.*;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import com.google.appengine.api.users.User;
+
+import com.google.appengine.api.users.UserService;
+
+import com.google.appengine.api.users.UserServiceFactory;
+
+ 
+
+import java.io.IOException;
+
+import java.util.Date;
+
+ 
+
+import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+
+ 
+
+public class OfySignGuestbookServlet extends HttpServlet {
+	
+	
+
+	static {
+
+        ObjectifyService.register(Greeting.class);
+
+    }
+	
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+
+                throws IOException {
+
+        UserService userService = UserServiceFactory.getUserService();
+
+        User user = userService.getCurrentUser();
+        
+        Greeting nuser = new Greeting(user, req.getParameter("content"), req.getParameter("guestbookName"));
+
+        ofy().save().entity(nuser).now();
+        
+ 
+        resp.sendRedirect("/ofyguestbook.jsp?guestbookName=" + req.getParameter("guestbookName"));
+
+    }
+
+}
